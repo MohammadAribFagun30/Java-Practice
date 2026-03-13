e9406f743b21889ca4b289d61dbc982eaa68526a
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-03-14 03:00:13 +0600

    hi

---
+++ src/com/company/lat.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class lat {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+    }
+
+}
