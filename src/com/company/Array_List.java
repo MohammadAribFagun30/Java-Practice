f59182ca7880a4f21b906573b4353019ca2ebe9f
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-03-15 19:41:23 +0600

    New

---
+++ src/com/company/Array_List.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.*;
+public class Array_List {
+     static void main() {
+      Scanner sc = new Scanner(System.in);
+
+     ArrayList<Integer> array = new ArrayList<>();
+
+     int size = sc.nextInt();
+      for(int i=0; i<size; i++)
+      {
+          array.add(sc.nextInt());
+      }
+         System.out.println("Array List is : ");
+         System.out.println(array);
+
+
+
+
+
+}
+
+    }
